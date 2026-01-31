3775852b946e7b925207643715ea8c8541352f92
michael-cornelius720 <jcornelius019@gmail.com>
2026-01-31 10:34:45 +0530

    feat: implement Progressive Web App (PWA) support using next-pwa.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+// @ts-ignore
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
+
